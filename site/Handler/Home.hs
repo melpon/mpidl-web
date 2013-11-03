a76b076510876cb6baa75199695c3deeb559cc6f
melpon <melt@e-ml.jp>
2013-11-03 19:36:36 +0900

    refactor

--- site/Handler/Home.hs
+++ site/Handler/Home.hs
@@ -31,10 +31,7 @@ service test {
 
 getHomeR :: Handler Y.Html
 getHomeR = do
-    let submission = Nothing :: Maybe (Y.FileInfo, T.Text)
-        handlerName = "getHomeR" :: T.Text
     Y.defaultLayout $ do
-        aDomId <- Y.newIdent
         Y.setTitle "MessagePack IDL Code Generator"
         $(widgetFile "homepage")
 
@@ -49,16 +46,9 @@ sanitize name =
                 'A' <= c && c <= 'Z'
     isDigit c = '0' <= c && c <= '9'
 
-postHomeR :: Handler (Y.ContentType, Y.Content)
-postHomeR = do
-  (maybe "noname" sanitize -> name, source, lang, namespace) <- Y.runInputPost $ (,,,)
-    <$> Y.iopt Y.textField "name"
-    <*> Y.ireq Y.textField "source"
-    <*> Y.ireq Y.textField "lang"
-    <*> Y.iopt Y.textField "namespace"
-
-  let tarname = [lt|#{name}.tar.bz2|]
-      idlname = [lt|#{name}.idl|]
+runMpidl :: Y.MonadIO m => T.Text -> T.Text -> T.Text -> Maybe T.Text -> (Shelly.FilePath -> Shelly.Sh a) -> m a
+runMpidl name source lang namespace resultFunc = do
+  let idlname = [lt|#{name}.idl|]
 
   let opts = map TL.fromStrict $ case (lang, namespace) of
         ("cpp",  Just ns) -> ["-n", ns]
@@ -66,16 +56,31 @@ postHomeR = do
         ("ruby", Just mn) -> ["-m", mn]
         _ -> []
   
-  archive <- Shelly.shelly $ do
+  Shelly.shelly $ do
     Shelly.withTmpDir $ \tmppath -> do
       let tmpName = tmppath </> name
       let tmpIdlname = tmppath </> Shelly.fromText idlname
-      let tmpTarname = tmppath </> Shelly.fromText tarname
       Shelly.writefile tmpIdlname $ TL.fromStrict source
       Shelly.run_ "msgpack-idl/bin/mpidl" $ [TL.fromStrict lang, "-o", Shelly.toTextIgnore tmpName, Shelly.toTextIgnore tmpIdlname] ++ opts
+      resultFunc tmppath
+
+postHomeR :: Handler (Y.ContentType, Y.Content)
+postHomeR = do
+    (maybe "noname" sanitize -> name, source, lang, namespace) <- Y.runInputPost $ (,,,)
+      <$> Y.iopt Y.textField "name"
+      <*> Y.ireq Y.textField "source"
+      <*> Y.ireq Y.textField "lang"
+      <*> Y.iopt Y.textField "namespace"
+
+    let tarname = [lt|#{name}.tar.bz2|]
+    archive <- runMpidl name source lang namespace (returnTar name tarname)
+
+    Y.addHeader "Content-Disposition" [st|attachment; filename="#{tarname}"|]
+    return ("application/x-bz2", Y.toContent archive)
+
+  where
+    returnTar name tarname tmppath = do
+      let tmpTarname = tmppath </> Shelly.fromText tarname
       Shelly.chdir tmppath $ do
         Shelly.run_ "tar" ["-cjf", tarname, TL.fromStrict name]
       Y.liftIO $ FS.readFile $ tmpTarname
-
-  Y.addHeader "Content-Disposition" [st|attachment; filename="#{tarname}"|]
-  return ("application/x-bz2", Y.toContent archive)
